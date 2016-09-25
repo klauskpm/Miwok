cf6fde2138dca69de25a75108167f7f4e94faffc
Klaus Kazlauskas <klausmachado@gmail.com>
2016-09-25 00:42:31 -0300

    Adding words to the list with Word class

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -14,21 +14,21 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        ArrayList<String> words = new ArrayList<String>();
+        ArrayList<Word> words = new ArrayList<Word>();
 
-        words.add("one");
-        words.add("two");
-        words.add("three");
-        words.add("four");
-        words.add("five");
-        words.add("six");
-        words.add("seven");
-        words.add("eight");
-        words.add("nine");
-        words.add("ten");
+        words.add(new Word("lutti", getString(R.string.translation_number_one)));
+        words.add(new Word("otiiko", getString(R.string.translation_number_two)));
+        words.add(new Word("tolookosu", getString(R.string.translation_number_three)));
+        words.add(new Word("oyyisa", getString(R.string.translation_number_four)));
+        words.add(new Word("massokka", getString(R.string.translation_number_five)));
+        words.add(new Word("temmokka", getString(R.string.translation_number_six)));
+        words.add(new Word("kenekaku", getString(R.string.translation_number_seven)));
+        words.add(new Word("kiwinta", getString(R.string.translation_number_eight)));
+        words.add(new Word("wo'e", getString(R.string.translation_number_nine)));
+        words.add(new Word("na'aacha", getString(R.string.translation_number_ten)));
 
-        ArrayAdapter<String> itemsAdapter =
-                new ArrayAdapter<String>(this, R.layout.list_item, words);
+        ArrayAdapter<Word> itemsAdapter =
+                new ArrayAdapter<Word>(this, R.layout.list_item, words);
 
         ListView listView = (ListView) findViewById(R.id.list);
         assert listView != null;
