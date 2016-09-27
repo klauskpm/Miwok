c079f8efcaedb559d55c25a73bd672701d56d5ce
Klaus Kazlauskas <klausmachado@gmail.com>
2016-09-26 22:21:13 -0300

    Colors images resources been setted

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -15,14 +15,14 @@ public class ColorsActivity extends AppCompatActivity {
 
         ArrayList<Word> words = new ArrayList<Word>();
 
-        words.add(new Word("weṭeṭṭi", getString(R.string.translation_color_red)));
-        words.add(new Word("chokokki", getString(R.string.translation_color_green)));
-        words.add(new Word("ṭakaakki", getString(R.string.translation_color_brown)));
-        words.add(new Word("ṭopoppi", getString(R.string.translation_color_gray)));
-        words.add(new Word("kululli", getString(R.string.translation_color_black)));
-        words.add(new Word("kelelli", getString(R.string.translation_color_white)));
-        words.add(new Word("ṭopiisә", getString(R.string.translation_color_dusty_yellow)));
-        words.add(new Word("chiwiiṭә", getString(R.string.translation_color_mustard_yellow)));
+        words.add(new Word("weṭeṭṭi", getString(R.string.translation_color_red), R.drawable.color_red));
+        words.add(new Word("chokokki", getString(R.string.translation_color_green), R.drawable.color_green));
+        words.add(new Word("ṭakaakki", getString(R.string.translation_color_brown), R.drawable.color_brown));
+        words.add(new Word("ṭopoppi", getString(R.string.translation_color_gray), R.drawable.color_gray));
+        words.add(new Word("kululli", getString(R.string.translation_color_black), R.drawable.color_black));
+        words.add(new Word("kelelli", getString(R.string.translation_color_white), R.drawable.color_white));
+        words.add(new Word("ṭopiisә", getString(R.string.translation_color_dusty_yellow), R.drawable.color_dusty_yellow));
+        words.add(new Word("chiwiiṭә", getString(R.string.translation_color_mustard_yellow), R.drawable.color_mustard_yellow));
 
         WordAdapter itemsAdapter =
                 new WordAdapter(this, R.layout.list_item, words);
