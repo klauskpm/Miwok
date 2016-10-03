053239f69b412f7f791dfcb4a785995f96250d0b
Klaus Kazlauskas <klausmachado@gmail.com>
2016-10-03 14:19:36 -0300

    Colors Fragment

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -1,114 +1,19 @@
 package com.example.android.miwok;
 
-import android.content.Context;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-
-import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
-    private MediaPlayer mMediaPlayer;
-    private AudioManager mAudioManager;
 
-    private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener =
-            new AudioManager.OnAudioFocusChangeListener() {
-                @Override
-                public void onAudioFocusChange(int focusChange) {
-                    switch(focusChange) {
-                        case AudioManager.AUDIOFOCUS_LOSS:
-                            releaseMediaPlayer();
-                            break;
-
-                        case AudioManager.AUDIOFOCUS_GAIN:
-                        case AudioManager.AUDIOFOCUS_GAIN_TRANSIENT:
-                            mMediaPlayer.start();
-                            break;
-
-                        case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
-                        case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
-                            mMediaPlayer.pause();
-                            mMediaPlayer.seekTo(0);
-                            break;
-
-                        default:
-                            releaseMediaPlayer();
-                            break;
-                    }
-                }
-            };
-
-    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
-        @Override
-        public void onCompletion(MediaPlayer mediaPlayer) {
-            releaseMediaPlayer();
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.word_list);
+        setContentView(R.layout.activity_category);
 
-        final ArrayList<Word> words = new ArrayList<Word>();
-
-        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-
-        words.add(new Word("weṭeṭṭi", getString(R.string.translation_color_red), R.raw.color_red, R.drawable.color_red));
-        words.add(new Word("chokokki", getString(R.string.translation_color_green), R.raw.color_green, R.drawable.color_green));
-        words.add(new Word("ṭakaakki", getString(R.string.translation_color_brown), R.raw.color_brown, R.drawable.color_brown));
-        words.add(new Word("ṭopoppi", getString(R.string.translation_color_gray), R.raw.color_gray, R.drawable.color_gray));
-        words.add(new Word("kululli", getString(R.string.translation_color_black), R.raw.color_black, R.drawable.color_black));
-        words.add(new Word("kelelli", getString(R.string.translation_color_white), R.raw.color_white, R.drawable.color_white));
-        words.add(new Word("ṭopiisә", getString(R.string.translation_color_dusty_yellow), R.raw.color_dusty_yellow,
-                R.drawable.color_dusty_yellow));
-        words.add(new Word("chiwiiṭә", getString(R.string.translation_color_mustard_yellow), R.raw.color_mustard_yellow,
-                R.drawable.color_mustard_yellow));
-
-        WordAdapter itemsAdapter =
-                new WordAdapter(this, R.layout.list_item, words, R.color.category_colors);
-
-        ListView listView = (ListView) findViewById(R.id.list);
-        assert listView != null;
-        listView.setAdapter(itemsAdapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                releaseMediaPlayer();
-                Word currentWord = words.get(i);
-
-                int audioFocusRequest = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
-                        AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
-
-                if (audioFocusRequest == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-                    mMediaPlayer = MediaPlayer.create(ColorsActivity.this, currentWord.getAudioResourceId());
-                    mMediaPlayer.start();
-
-                    mMediaPlayer.setOnCompletionListener(mCompletionListener);
-                }
-            }
-        });
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        releaseMediaPlayer();
-    }
-
-    private void releaseMediaPlayer() {
-        if (mMediaPlayer != null) {
-            mMediaPlayer.release();
-
-            mMediaPlayer = null;
-        }
-
-        mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new ColorsFragment())
+                .commit();
     }
 }
---
+++ app/src/main/java/com/example/android/miwok/ColorsFragment.java
@@ -0,0 +1,129 @@
+package com.example.android.miwok;
+
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ColorsFragment extends Fragment {
+
+    private MediaPlayer mMediaPlayer;
+    private AudioManager mAudioManager;
+
+    private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener =
+            new AudioManager.OnAudioFocusChangeListener() {
+                @Override
+                public void onAudioFocusChange(int focusChange) {
+                    switch (focusChange) {
+                        case AudioManager.AUDIOFOCUS_LOSS:
+                            releaseMediaPlayer();
+                            break;
+
+                        case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
+                        case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
+                            mMediaPlayer.pause();
+                            mMediaPlayer.seekTo(0);
+                            break;
+
+                        case AudioManager.AUDIOFOCUS_GAIN:
+                        case AudioManager.AUDIOFOCUS_GAIN_TRANSIENT:
+                            mMediaPlayer.start();
+                            break;
+
+                        default:
+                            releaseMediaPlayer();
+                            break;
+                    }
+                }
+            };
+
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            releaseMediaPlayer();
+        }
+    };
+
+    public ColorsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.word_list, container, false);
+
+        final ArrayList<Word> words = new ArrayList<Word>();
+
+        mAudioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
+
+        words.add(new Word("weṭeṭṭi", getString(R.string.translation_color_red), R.raw.color_red, R.drawable.color_red));
+        words.add(new Word("chokokki", getString(R.string.translation_color_green), R.raw.color_green, R.drawable.color_green));
+        words.add(new Word("ṭakaakki", getString(R.string.translation_color_brown), R.raw.color_brown, R.drawable.color_brown));
+        words.add(new Word("ṭopoppi", getString(R.string.translation_color_gray), R.raw.color_gray, R.drawable.color_gray));
+        words.add(new Word("kululli", getString(R.string.translation_color_black), R.raw.color_black, R.drawable.color_black));
+        words.add(new Word("kelelli", getString(R.string.translation_color_white), R.raw.color_white, R.drawable.color_white));
+        words.add(new Word("ṭopiisә", getString(R.string.translation_color_dusty_yellow), R.raw.color_dusty_yellow,
+                R.drawable.color_dusty_yellow));
+        words.add(new Word("chiwiiṭә", getString(R.string.translation_color_mustard_yellow), R.raw.color_mustard_yellow,
+                R.drawable.color_mustard_yellow));
+
+        WordAdapter itemsAdapter =
+                new WordAdapter(getActivity(), R.layout.list_item, words, R.color.category_colors);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
+        assert listView != null;
+        listView.setAdapter(itemsAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                releaseMediaPlayer();
+                Word currentWord = words.get(i);
+
+                int audioFocusRequest = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
+                        AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                if (audioFocusRequest == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    mMediaPlayer = MediaPlayer.create(getActivity(), currentWord.getAudioResourceId());
+                    mMediaPlayer.start();
+
+                    mMediaPlayer.setOnCompletionListener(mCompletionListener);
+                }
+            }
+        });
+
+        return rootView;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        releaseMediaPlayer();
+    }
+
+    private void releaseMediaPlayer() {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.release();
+
+            mMediaPlayer = null;
+        }
+
+        mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
+    }
+
+}
